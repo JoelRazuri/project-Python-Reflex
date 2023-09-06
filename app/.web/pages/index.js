ea28bc2cfa1965e1005d3bccc987b7259c137dc3
Joel <jrazuri98@hotmail.com>
2023-09-06 00:24:13 -0300

    index.js

--- app/.web/pages/index.js
+++ app/.web/pages/index.js
@@ -38,25 +38,33 @@ export default function Component() {
   <Container>
   <Box>
   <Box sx={{"marginY": "1em"}}>
-  <Box sx={{"textAlign": "rigth"}}>
-  {`Why is Reflex?`}
+  <Box sx={{"textAlign": "right"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#F5EFFE", "marginLeft": "20%"}}>
+  {`What is Reflex?`}
+</Text>
 </Box>
   <Box sx={{"textAlign": "left"}}>
-  {`A way to build wewb apps in pure Python`}
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#DEEAFD", "marginRight": "20%"}}>
+  {`A way to build web apps in pure Python!`}
+</Text>
 </Box>
 </Box>
   <Box sx={{"marginY": "1em"}}>
-  <Box sx={{"textAlign": "rigth"}}>
+  <Box sx={{"textAlign": "right"}}>
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#F5EFFE", "marginLeft": "20%"}}>
   {`What can I make with it?`}
+</Text>
 </Box>
   <Box sx={{"textAlign": "left"}}>
-  {`Anything form a simple website yo a complex web app!`}
+  <Text sx={{"padding": "1em", "borderRadius": "5px", "marginY": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "bg": "#DEEAFD", "marginRight": "20%"}}>
+  {`Anything from a simple website to a complex web app!`}
+</Text>
 </Box>
 </Box>
 </Box>
   <HStack>
-  <Input placeholder={`Ask a question`} type={`text`}/>
-  <Button>
+  <Input placeholder={`Ask a question`} sx={{"borderWidth": "1px", "padding": "1em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}} type={`text`}/>
+  <Button sx={{"bg": "#CEFFEE", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}}>
   {`Ask`}
 </Button>
 </HStack>
