65fb598e45ab5a0fc61ed8bc934ca53db1f61e30
Joel <jrazuri98@hotmail.com>
2023-09-06 00:24:45 -0300

    Agregando estilos

--- app/app/app.py
+++ app/app/app.py
@@ -2,7 +2,7 @@
 from rxconfig import config
 
 import reflex as rx
-
+from app import style
 
 
 
@@ -13,38 +13,52 @@ class State(rx.State):
 
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="rigth"),
-        rx.box(answer, text_align="left"),
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
         margin_y="1em",
     )
+
 
 def chat() -> rx.Component:
     qa_pairs = [
         (
-            "Why is Reflex?",
-            "A way to build wewb apps in pure Python",
+            "What is Reflex?",
+            "A way to build web apps in pure Python!",
         ),
         (
             "What can I make with it?",
-            "Anything form a simple website yo a complex web app!",
-        )
+            "Anything from a simple website to a complex web app!",
+        ),
     ]
     return rx.box(
         *[
-            qa(question, answer) for question, answer in qa_pairs
+            qa(question, answer)
+            for question, answer in qa_pairs
         ]
-    ) 
+    )
+
 
 def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Ask a question"),
-        rx.button("Ask")
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button("Ask", style=style.button_style),
     )
+
 
 def index() -> rx.Component:
     return rx.container(
         chat(),
-        action_bar())
+        action_bar(),
+    )
 
 
 # Add state and page to the app.
